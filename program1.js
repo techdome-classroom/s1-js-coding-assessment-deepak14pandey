d0c400e6b5fde8d6d1413e3d97806621d98c4e75
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:16:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,7 +5,9 @@ const getTotalIsles = function (grid) {
  const cols = grid[0].length;
  let islandCount = 0;
 
- function dfs(r, c)
+ function dfs(r, c) {
+  if(r<0 || c<0 )
+ }
 
   // write your code here
 
