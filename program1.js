4e6f49b10171e91c5c0d97200fc5c919cd797f3b
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:15:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,8 @@ const getTotalIsles = function (grid) {
  if(grid.length == 0) return 0;
 
  const rows = grid.length;
- const cols = gri
+ const cols = grid[0].length;
+ 
 
   // write your code here
 
