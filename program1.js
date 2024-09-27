8eef84042b95f2e5a684f6d2eb9ab3c4d381ce2a
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:21:34 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -21,7 +21,7 @@ const getTotalIsles = function (grid) {
 for(let r =0; r< rows; r++){
   for(let c =0; c< cols; c++){
     if(grid[r][c] ==="L"){
-      islandcount++;
+      islandCount++;
     }
   }
 }
