0b3f5e3287b141432739ab021ec129d579fa1c34
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:45:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,50 +1,8 @@
-
-function countIslands(grid) {
-  if (grid.length === 0) return 0;
-
-  const rows = grid.length;
-  const cols = grid[0].length;
-  let islandCount = 0;
-
-  
-  function dfs(r, c) {
-      
-      if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') {
-          return;
-      }
-      
-     
-      grid[r][c] = 'W';
-
-      
-      dfs(r + 1, c); 
-      dfs(r - 1, c); 
-      dfs(r, c + 1); 
-      dfs(r, c - 1); 
-  }
-
-  
-  for (let r = 0; r < rows; r++) {
-      for (let c = 0; c < cols; c++) {
-          if (grid[r][c] === 'L') {
-              
-              islandCount++;
-              
-              
-              dfs(r, c);
-          }
-      }
-  }
-
-  return islandCount;
-}
+const getTotalIsles = function (grid) {
 
 
-const grid = [
-  ["L","L","L","L","W"],
-  ["L","L","W","L","W"],
-  ["L","L","W","W","W"],
-  ["W","W","W","W","W"],
-];
+  // write your code here
 
-console.log(countIslands(grid)); 
+};
+
+module.exports = getTotalIsles;
