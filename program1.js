5b0a817f303f51a88a1fafe673d6269d47aaf3d8
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:21:12 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -19,7 +19,9 @@ const getTotalIsles = function (grid) {
 }
 
 for(let r =0; r< rows; r++){
-  for(let c =0; c< cols)
+  for(let c =0; c< cols; c++){
+    if(grid[r][c] ==="L")
+  }
 }
   // w;rite your code here
 
