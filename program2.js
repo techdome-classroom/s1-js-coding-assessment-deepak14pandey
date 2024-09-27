969785e2de1f931eb62e66acac755a17cd4388d3
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:36:01 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -23,7 +23,7 @@ function isMatch(message, pattern) {
       }
   }
 
-  // Fill the DP table
+  
   for (let i = 1; i <= m; i++) {
       for (let j = 1; j <= p; j++) {
           if (pattern[j - 1] === '*') {
