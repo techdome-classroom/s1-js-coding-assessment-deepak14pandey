7e7874f1e6f8455c7585a7528ef57ce6a336f962
DeepakPandey <deepakgpandey05@gmail.com>
2024-09-27 12:25:39 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -62,7 +62,7 @@ function countIslands(grid) {
       dfs(r, c - 1); // Left
   }
 
-  // Loop through each cell in the grid
+  
   for (let r = 0; r < rows; r++) {
       for (let c = 0; c < cols; c++) {
           if (grid[r][c] === 'L') {
